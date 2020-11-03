7e29604937c21893a419f0c9cffa4575ab94a70f
Andrew Kamal <kamalandrew55@gmail.com>
2020-11-03 12:18:38 -0500

    Update CryptoNoteAdapter.cpp

--- src/CryptoNoteWrapper/CryptoNoteAdapter.cpp
+++ src/CryptoNoteWrapper/CryptoNoteAdapter.cpp
@@ -107,7 +107,7 @@ bool CryptoNoteAdapter::isValidPaymentId(const QString& _paymentId) const {
 }
 
 QString CryptoNoteAdapter::getCurrencyTicker() const {
-  return "LUK";
+  return "LNR";
 }
 
 quint64 CryptoNoteAdapter::getMinimalFee() const {
